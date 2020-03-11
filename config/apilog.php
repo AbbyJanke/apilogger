fdda8997b1c741a55ea9057d426800c1df1f54c3
Abby <me@abbyjanke.com>
2020-03-10 19:25:32 -0700

    Allow excluding input

--- config/apilog.php
+++ config/apilog.php
@@ -4,4 +4,11 @@ return [
     //currently supported drivers are 'db' and 'file'
     "driver" => "file",
     "filename" => "api-{Y-m-d}.log"
+
+    // An array of request input data to be excluded from logs
+    // recommended for passwords and information typically encrypted
+    "exclude" => [
+    	'password',
+    	'password_confirmation'
+    ]
 ];
